1c51f639ca375f447cc423f99d73164976bc316f
kran <kierancollins@outlook.co.nz>
2026-07-24 17:04:06 +1200

    pot
    added a pot

---
+++ Scripts/shitty_temp_char.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+var near_pot: bool = false
+
+const SPEED = 300.0
+
+func _process(delta: float) -> void:
+	print(near_pot)
+
+func _physics_process(_delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	near_pot = true
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	near_pot = false
