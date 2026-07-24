3306d0716b2862acb2e3d0ed58112f38e3d349cc
kran <kierancollins@outlook.co.nz>
2026-07-24 21:59:00 +1200

    text
    made text change when in menu

--- Scripts/lollie_making_system.gd
+++ Scripts/lollie_making_system.gd
@@ -8,4 +8,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if $ShittyTempChar.in_pot_menu == true:
+		$Text.text = "Press E to close menu"
+	else:
+		$Text.text = "Press E to start mixing ingrediants"
