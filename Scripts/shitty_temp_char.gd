948a65e02085ae9167014668f6cc72a0a0b3aff7
kran <kierancollins@outlook.co.nz>
2026-07-24 19:22:03 +1200

    interact keybind
    added an interact keybind

--- Scripts/shitty_temp_char.gd
+++ Scripts/shitty_temp_char.gd
@@ -5,8 +5,12 @@ var near_pot: bool = false
 const SPEED = 300.0
 
 func _process(delta: float) -> void:
-	print(near_pot)
-
+	
+	if Input.is_action_just_pressed("interact"):
+		if near_pot == true:
+			print("interacting")
+		else:
+			print("failed to interact")
 func _physics_process(_delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -22,6 +26,8 @@ func _physics_process(_delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	near_pot = true
+	print(near_pot)
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	near_pot = false
+	print(near_pot)
