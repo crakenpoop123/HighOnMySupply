4049be68b523032371d44ef5c0a212dbac7041d8
kran <kierancollins@outlook.co.nz>
2026-07-26 00:22:51 +1200

    misc fixes

--- Scripts/lolly_making_menu.gd
+++ Scripts/lolly_making_menu.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	$RichTextLabel.text = "Gummy worms stock: " + str(Globals.gummy_worm_stock)
 
 
