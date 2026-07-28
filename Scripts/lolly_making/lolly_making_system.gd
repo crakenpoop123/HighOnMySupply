b116fcbd381516a36d9a56f7884959c6e44c4b60
krannnnn <kierancollins@outlook.co.nz>
2026-07-28 13:14:04 +1200

    comments

--- Scripts/lolly_making/lolly_making_system.gd
+++ Scripts/lolly_making/lolly_making_system.gd
@@ -2,16 +2,14 @@ extends Node2D
 
 const COOKING_MENU = preload("res://Scenes/lolly_making/lolly_making_menu.tscn")
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	$Text.text = "Press E to start mixing ingrediants"
+func _process(_delta: float) -> void: 
+	$Text.text = "Press E to start mixing ingrediants" # Origially the text changed so thats why this is here
 	
-func cooking_menu_inst():
+func cooking_menu_inst(): #Make the menu appear
 	var cooking_menu = COOKING_MENU.instantiate()
 	$Menus.add_child(cooking_menu)
 	
