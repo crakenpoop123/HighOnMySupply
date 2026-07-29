7194609c38a05bea6fa8790c1c508a346e0b5212
crakenpoop123 <gerracl@whs.school.nz>
2026-07-29 12:19:22 +1200

    Begins a farm plot
    Just a pink square rn

--- Scripts/menus/main_menu.gd
+++ Scripts/menus/main_menu.gd
@@ -13,3 +13,7 @@ func _process(delta: float) -> void:
 
 func _on_button_button_up() -> void:
 	get_tree().change_scene_to_file("res://Scenes/lolly_making/lolly_making_system.tscn")
+
+
+func _on_sugar_making_button_up() -> void:
+	get_tree().change_scene_to_file("res://Scenes/sugar_making/sugar_making_system.tscn")
