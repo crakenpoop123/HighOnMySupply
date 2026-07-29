5c5d8a8a9969204196814bda5295ff82bdb702fc
krannnnn <kierancollins@outlook.co.nz>
2026-07-29 12:04:31 +1200

    Inventory
    very simple inventory that displays stock of items

--- Scripts/menus/inventory.gd
+++ Scripts/menus/inventory.gd
@@ -8,9 +8,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
-
+	$Main/SugarStock.text = str("Sugar: ", Globals.sugar)
+	$Main/GelatinStock.text = str("Gelatin: ", Globals.gelatin)
+	$Main/GummyStock.text = str("Gummy worms: ", Globals.gummy_worm_stock)
+	$Main/Money.text = str("Money: ", Globals.money)
+	
 func _on_close_button_button_up() -> void:
 	self.queue_free()
 	Globals.in_inventory = false
