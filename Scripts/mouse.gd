db104efde073192677a77f0b0289101df8d01ac0
krannnnn <kierancollins@outlook.co.nz>
2026-07-31 15:17:03 +1200

    barely working mouse + some assets

--- Scripts/mouse.gd
+++ Scripts/mouse.gd
@@ -5,6 +5,7 @@ var holding_click = false
 var holding = false
 var mouse_in_area = false
 var area_name: Node2D
+var also_area_name
 @onready var mouse_pos = get_global_mouse_position()
 
 func _process(_delta: float) -> void:
@@ -13,7 +14,7 @@ func _process(_delta: float) -> void:
 		holding_click = true
 	if Input.is_action_just_released("click"):
 		holding_click = false
-	
+	print(mouse_in_area)
 	$MouseArea/MouseCollision.global_position = mouse_pos
 	if Globals.can_drag == true:
 		if holding == true and area_name.get_parent(): # MOVES THE ITEM WHEN YOU ARE HOLDING IT
@@ -25,15 +26,14 @@ func _process(_delta: float) -> void:
 			if mouse_in_area == true:# WHEN YOU ARE IN AN AREA
 				if holding_click == true: # AND YOU ARE HOLDING CLICK
 					holding = true # START DRAGGING
-	
+					
+	if holding == false:
+		if $MouseArea.has_overlapping_areas() == true:
+			mouse_in_area = true 
+	if holding == false:
+		if $MouseArea.has_overlapping_areas() == false:
+			mouse_in_area = false
+			area_name = EMPTY
 
 func _on_mouse_area_area_shape_entered(_area_rid: RID, area: Area2D, _area_shape_index: int, _local_shape_index: int) -> void:
-	if holding == false:
-		mouse_in_area = true
-		area_name = area.get_parent()
-
-
-func _on_mouse_area_area_shape_exited(_area_rid: RID, _area: Area2D, _area_shape_index: int, _local_shape_index: int) -> void:
-	if holding == false:
-		mouse_in_area = false
-		area_name = EMPTY
+	area_name = area.get_parent() # THIS DOESN'T WORK THAT GREAT BUT IT SHOULDN'T BE AN ISSUE 
