2048004578afae2015eb4e9b46bf0abaecdfc931
krannnnn <kierancollins@outlook.co.nz>
2026-08-06 15:13:10 +1200

    images and sugarcane
    changed some images
    also started a minigame for harvesting sugarcane
    *BLEEP* YOU ZAVIER

--- scripts/lolly_making/water.gd
+++ scripts/lolly_making/water.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
 var inside_pot = false
+var dropped_into_pot = false
+var size = 32
 @onready var mouse = $"../Mouse"
+
+#func _process(delta: float) -> void:
+	#if dropped_into_pot == true: # CODE TO CHANGE IT TO A DIFFERENT SPRITE WHEN SOMETHING ELSE IS MADE
+	#	$IngrediantSprite.texture.region = Rect2(64, 0, size, size)
 
 func _on_ingrediant_area_area_shape_entered(_area_rid: RID, area: Area2D, _area_shape_index: int, _local_shape_index: int) -> void:
 	if area.name == "SaucepanArea":
