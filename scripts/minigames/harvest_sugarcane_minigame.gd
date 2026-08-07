be66f7d8dc487fcac99bbe38b694108f0748b5a2
crakenpoop123 <gerracl@whs.school.nz>
2026-08-07 15:12:10 +1200

    Knife rotates with mouse

--- scripts/minigames/harvest_sugarcane_minigame.gd
+++ scripts/minigames/harvest_sugarcane_minigame.gd
@@ -10,6 +10,8 @@ func _process(_delta: float) -> void:
 		Globals.change_scene(false)
 	
 	if mouse.area_name: # IF THE NAME IS NOT NIL
+		if mouse.area_name.name == "CaneKnife" and mouse.holding_click:
+			rotate_to_mouse($CaneKnife, 0)
 		if mouse.area_name.name != "Sugarcane": # IF THE NAME IS NOT SUGARCANE
 			Globals.can_drag = true
 		else:
@@ -18,18 +20,18 @@ func _process(_delta: float) -> void:
 	#print(mouse.area_name, " ", Globals.can_drag)
 
 func _physics_process(delta: float) -> void:
-	rotate_to_mouse($CaneKnife)
+	
 	for ray in $CaneKnife/Blade/BladeRays.get_children():
-		rotate_to_mouse(ray)
+		rotate_to_mouse(ray, PI/2)
 		#print($CaneKnifeOLD/Blade/BladeRay.get_collider())
 		if ray.is_colliding():
 			print("Blade colliding with: ", ray.get_collider().get_parent().name)
 			if ray.get_collider().get_parent().name == "Sugarcane" and mouse.movement_speed >= min_chop_speed:
 				hit_sugarcane = true
 
-func rotate_to_mouse(body):
+func rotate_to_mouse(body, offset):
 	print(body.rotation)
-	body.rotation = mouse.movement_dir + PI/2
+	body.rotation = mouse.movement_dir + offset
 
 func _on_blade_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
 	print(mouse.movement_speed)
