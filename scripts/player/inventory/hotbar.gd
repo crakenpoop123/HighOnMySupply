3ce5bb857cff0219d07c6b4abef4bdc9d9e3e72d
crakenpoop123 <gerracl@whs.school.nz>
2026-08-10 11:18:19 +1200

    Allow the player to pick what slot is active

--- scripts/player/inventory/hotbar.gd
+++ scripts/player/inventory/hotbar.gd
@@ -3,6 +3,10 @@ extends Control
 @onready var item = preload("res://scenes/player/inventory/hotbar_item.tscn")
 var num_slots = 10
 
+var slot_focused = 0
+
+var opacity = 0.75
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for slot in range(num_slots):
@@ -13,4 +17,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var slot_number = 0
+	for slot in get_node("HotbarGrid").get_children():
+		slot.label = str(slot_number)
+		if slot_number == slot_focused:
+			slot.modulate = Color(1, 1, 1, 1)
+		else:
+			slot.modulate = Color(1, 1, 1, opacity)
+		slot_number += 1
