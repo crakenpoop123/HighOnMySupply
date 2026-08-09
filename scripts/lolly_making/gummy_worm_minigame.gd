ff8e46c68256e7ef1484f9cd13c2cf65d2bb4150
krannnnn <kierancollins@outlook.co.nz>
2026-08-10 11:56:34 +1200

    merging the two gummy worm minigames

--- scripts/lolly_making/gummy_worm_minigame.gd
+++ scripts/lolly_making/gummy_worm_minigame.gd
@@ -1,32 +1,27 @@
 extends Node2D
 
 @onready var mouse = $Mouse
-@onready var sugar = $Sugar
-@onready var saucepan = $Saucepan
-@onready var gelatin = $Gelatin
-@onready var water = $Water
+@onready var sugar = $Table/Sugar
+@onready var saucepan = $Table/Saucepan
+@onready var gelatin = $Table/Gelatin
+@onready var water = $Table/Water
 
 
 var empty_saucepan = load("res://assets/misc/saucepan.png")
 var water_saucepan = load("res://assets/misc/water-saucepan.png")
 var all_ingrediants_in_pot = false
 var ingredients_in_pot = 0
-
-func _ready() -> void:
-	globals.can_drag = false # SET THE MOUSE DRAG TO FALSE SO THAT CODE TO DRAG CAN BE ADDED HERE BECAUSE ANNOYING THINGS HAPPEN
-	gelatin.visible = false
+var starting_to_close: bool = false
 	
 func _process(_delta: float) -> void:
-	#print("inside pot", sugar.inside_pot)
-	#print("everything in pot", all_ingrediants_in_pot)
 	if ingredients_in_pot == 3:
-		all_ingrediants_in_pot = true
-		globals.saucepan_on_table = false
-		globals.change_scene(false)
+		if starting_to_close == false:
+			all_ingrediants_in_pot = true
+			move_to_stove()
+			starting_to_close = true
 	elif ingredients_in_pot == 2:
 		gelatin.visible = true
 
-	#print(ingredients_in_pot)
 
 	check_for_move()
 	check_for_drop()
@@ -43,32 +38,38 @@ func _process(_delta: float) -> void:
 
 func check_for_move():
 	if mouse.holding == false: # MOVES THE ITEM WHEN YOU ARE HOLDING IT
-		#mouse.area_name.global_position = get_global_mouse_position() # MOVE THE ITEM
 		if mouse.holding_click == false: # IF YOU ARENT HOLDING CLICK
 			mouse.holding = false # DROP IT
 			if sugar.inside_pot == true:
-				$Sugar/IngrediantArea.queue_free()
+				$Table/Sugar/IngrediantArea.queue_free()
 				sugar.reparent(saucepan)
 				sugar.dropped_into_pot = true
 				ingredients_in_pot += 1 # Why doesnt this add 1 infinitely??
 			if gelatin.inside_pot == true:
-				$Gelatin/IngrediantArea.queue_free()
+				$Table/Gelatin/IngrediantArea.queue_free()
 				gelatin.reparent(saucepan)
 				gelatin.dropped_into_pot = true
 				ingredients_in_pot += 1 # Why doesnt this add 1 infinitely??
 			if water.inside_pot == true:
-				$Saucepan/SaucepanSprite.texture = water_saucepan
-				$Water/IngrediantArea.queue_free()
-				$Water/IngrediantSprite.queue_free()
+				$Table/Saucepan/SaucepanSprite.texture = water_saucepan
+				$Table/Water/IngrediantArea.queue_free()
+				$Table/Water/IngrediantSprite.queue_free()
 				water.reparent(saucepan)
 				water.dropped_into_pot = true
 				ingredients_in_pot += 1 # Why doesnt this add 1 infinitely??
 				
-				
-			
-
 func check_for_drop(): # Somehow I don't understand my own code so just don't touch this becuase if it breaks I can't fix it
 	if mouse.holding == false: # IF YOU ARENT HOLDING ANYTHING
 		if mouse.mouse_in_area == true:# WHEN YOU ARE IN AN AREA
 			if mouse.holding_click == true: # AND YOU ARE HOLDING CLICK
 				mouse.holding = true # START DRAGGING # Should't this be stop dragging???
+
+	
+#func _on_close_timer_timeout() -> void:
+	
+	#globals.saucepan_on_table = false
+	#globals.change_scene(false)
+	
+func move_to_stove():
+	var tween = get_tree().create_tween()
+	tween.tween_property($Camera2D, "position", Vector2(-600, 0), 1.0)
