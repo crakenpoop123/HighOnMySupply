b72878e75c78c62285f095b788164471951a5b57
krannnnn <kierancollins@outlook.co.nz>
2026-08-14 12:53:37 +1200

    gummy worm minigame
    gummy worm minigame camera moves when it should

--- scripts/lolly_making/saucepan.gd
+++ scripts/lolly_making/saucepan.gd
@@ -2,10 +2,10 @@ extends Node2D
 
 var on_oven = false
 
-func _on_ingrediant_area_area_shape_entered(_area_rid: RID, area: Area2D, _area_shape_index: int, _local_shape_index: int) -> void:
-	if area.parent.name == "Stove":
+func _on_saucepan_area_body_entered(body: StaticBody2D) -> void:
+	if body.name == "StoveBody":
 		on_oven = true
 
-func _on_ingrediant_area_area_shape_exited(_area_rid: RID, area: Area2D, _area_shape_index: int, _local_shape_index: int) -> void:
-	if area.parent.name == "Stove":
+func _on_saucepan_area_body_exited(body: StaticBody2D) -> void:
+	if body.name == "StoveBody":
 		on_oven = false
