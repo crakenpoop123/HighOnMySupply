2dfd820b3f2343e40f5632ff6c19bfcc54a665e2
krannnnn <kierancollins@outlook.co.nz>
2026-08-14 15:00:37 +1200

    minigame

--- scripts/lolly_making/gummy_cooking_minigame.gd
+++ scripts/lolly_making/gummy_cooking_minigame.gd
@@ -1,12 +1,8 @@
 extends Node2D
 
 @onready var mouse = $Mouse
-@onready var sugar = $Table/Sugar
 @onready var saucepan = $Table/Saucepan
-@onready var gelatin = $Table/Gelatin
-@onready var water = $Table/Water
 
-var animation_playing = false
 var empty_saucepan = load("res://assets/misc/saucepan.png")
 var water_saucepan = load("res://assets/misc/water-saucepan.png")
 var all_ingrediants_in_pot = false
@@ -15,14 +11,10 @@ var starting_to_close: bool = false
 
 func _ready() -> void:
 	$Mouse/Camera.global_position = Vector2(-300, 0)
-
+	saucepan.in_cooking_scene = true
+	
 func _process(_delta: float) -> void:
-	if ingredients_in_pot == 3:
-		if starting_to_close == false:
-			all_ingrediants_in_pot = true
-	elif ingredients_in_pot == 2:
-		gelatin.visible = true
-
+	
 	check_for_move()
 	check_for_drop()
 
@@ -30,28 +22,7 @@ func check_for_move():
 	if mouse.holding == false: # MOVES THE ITEM WHEN YOU ARE HOLDING IT
 		if mouse.holding_click == false: # IF YOU ARENT HOLDING CLICK
 			mouse.holding = false # DROP IT
-			if sugar.inside_pot == true:
-				$Table/Sugar/IngrediantArea.queue_free()
-				sugar.reparent(saucepan)
-				sugar.dropped_into_pot = true
-				ingredients_in_pot += 1 # Why doesnt this add 1 infinitely??
-			if gelatin.inside_pot == true:
-				$Table/Gelatin/IngrediantArea.queue_free()
-				gelatin.reparent(saucepan)
-				gelatin.dropped_into_pot = true
-				ingredients_in_pot += 1 # Why doesnt this add 1 infinitely??
-			if water.inside_pot == true:
-				$Table/Saucepan/SaucepanSprite.texture = water_saucepan
-				$Table/Water/IngrediantArea.queue_free()
-				$Table/Water/IngrediantSprite.queue_free()
-				water.reparent(saucepan)
-				water.dropped_into_pot = true
-				ingredients_in_pot += 1 # Why doesnt this add 1 infinitely??
-			if all_ingrediants_in_pot == true:
-				if saucepan.on_oven == true:
-					move_to_stove()
-					$AnimationPlayer.play("stove")
-					$Timer.start()
+			
 				
 func check_for_drop(): # Somehow I don't understand my own code so just don't touch this becuase if it breaks I can't fix it
 	if mouse.holding == false: # IF YOU ARENT HOLDING ANYTHING
