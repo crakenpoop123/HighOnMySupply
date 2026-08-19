c0fdfead6f56634a5c6ef0883677d744dae807a4
hydeza <hydeza@whs.school.nz>
2026-08-20 09:37:26 +1200

    push

--- scripts/levels/outside.gd
+++ scripts/levels/outside.gd
@@ -1,11 +1,20 @@
 extends Node2D
-
+@export var player: Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Tilemaps/Collisions.hide()
-
-
+	$"Tilemaps/Bottom Layer/Buildings".modulate.a = 100
+	$"Tilemaps/Bottom Layer/Buildings Bottom".modulate.a = 100
+	
+	$"Tilemaps/Second Layer/Buildings2".modulate.a = 100
+	$"Tilemaps/Second Layer/Buildings2 Bottom".modulate.a = 100
+	
+	$"Tilemaps/Third Layer/Buildings3".modulate.a = 100
+	$"Tilemaps/Third Layer/Buildings3 Bottom".modulate.a = 100
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	print(player.global_position.y)
+	if player.global_position.y < -465:
+		$"Tilemaps/Bottom Layer/Buildings".modulate.a = 0
+		$"Tilemaps/Bottom Layer/Buildings Bottom".modulate.a = 0
