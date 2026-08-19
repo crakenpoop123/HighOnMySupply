7f24a296a8ee21d4013d8f8ac2a14b5c92c9e270
krannnnn <kierancollins@outlook.co.nz>
2026-08-20 09:35:43 +1200

    more comments + deleting unneeded scripts

--- scripts/lolly_making/saucepan.gd
+++ scripts/lolly_making/saucepan.gd
@@ -6,11 +6,11 @@ var on_mold = false
 var size = 32
 	
 func _ready() -> void:
-	$SaucepanSprite.texture.region = Rect2(32, 0, size, size*2)
+	$SaucepanSprite.texture.region = Rect2(32, 0, size, size*2) # SET THE AREA OF THE ATLAS TEXTURE
 	if in_cooking_scene == true:
-		$SaucepanSprite.texture = load("res://assets/full-saucepan-spritesheet.png")
+		$SaucepanSprite.texture = load("res://assets/full-saucepan-spritesheet.png") # WHAT?
 		
-func _on_saucepan_area_body_entered(body: StaticBody2D) -> void:
+func _on_saucepan_area_body_entered(body: StaticBody2D) -> void: # DETECT WHEN ENTERING AREAS
 	if body.name == "StoveBody":
 		on_oven = true
 	if body.name == "TableBody":
@@ -19,7 +19,7 @@ func _on_saucepan_area_body_entered(body: StaticBody2D) -> void:
 		on_mold = true
 		$"../Mold".change_sprite()
 		
-func _on_saucepan_area_body_exited(body: StaticBody2D) -> void:
+func _on_saucepan_area_body_exited(body: StaticBody2D) -> void: # DETECT WHEN EXITING AREAS
 	if body.name == "StoveBody":
 		on_oven = false
 	if body.name == "TableBody":
@@ -27,6 +27,5 @@ func _on_saucepan_area_body_exited(body: StaticBody2D) -> void:
 	if body.name == "MoldBody":
 		on_mold = false
 		
-func empty():
-	print("Saucepan is empty")
+func empty(): # MAKE THE SAUCEPAN SPRITE EMPTY
 	$SaucepanSprite.texture = load("res://assets/misc/saucepan.png")
