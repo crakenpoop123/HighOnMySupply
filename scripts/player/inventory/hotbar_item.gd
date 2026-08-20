fb41811e3da063153a6209dfafc29fc75918453f
crakenpoop123 <gerracl@whs.school.nz>
2026-08-20 12:30:47 +1200

    More hotbar comments

--- scripts/player/inventory/hotbar_item.gd
+++ scripts/player/inventory/hotbar_item.gd
@@ -1,5 +1,6 @@
 extends TextureRect
 
+# Variables
 var label = "0"
 var mouse_touching = false
 var item_icon = null
@@ -13,15 +14,21 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	# Set the text
 	$ItemQuantity.text = label
+	
+	# Show the item icon
 	$SlotItem.visible = item_icon != null
+	
+	# Set the icon's texture
 	if item_icon:
 		$SlotItem.texture = item_icon
 
 
+# Detect when the mouse touches the slot
 func _on_slot_area_mouse_entered() -> void:
 	mouse_touching = true
 
-
+# Detect when the mosue stops touching the slot
 func _on_slot_area_mouse_exited() -> void:
 	mouse_touching = false
