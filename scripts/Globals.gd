4d089e524144099ab0aa452fc5d481e2f899a73c
crakenpoop123 <gerracl@whs.school.nz>
2026-08-28 09:52:57 +1200

    Added building obstruction
     - You can no longer place buildings over top of each other

--- scripts/Globals.gd
+++ scripts/Globals.gd
@@ -6,7 +6,7 @@ var in_cooking_menu = false
 var in_menu = false
 var in_inventory = false
 var just_in_inventory = false
-var interactable_parents = ["Farm", "Machinery", "Saucepan", "Door", "ShedDoor", "DairyDoor", "DoorToBasement", "DoorToStore"]
+var interactable_parents = ["Farm", "Machinery", "Saucepan", "Door", "ShedDoor", "DairyDoor", "DoorToBasement", "DoorToStore", "MachineryThings"]
 
 # Scene *wow*
 var scene = null
