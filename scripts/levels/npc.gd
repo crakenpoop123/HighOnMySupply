af964c54c72a708c683472b94f3d5463ea14c3f8
krannnnn <kierancollins@outlook.co.nz>
2026-08-31 11:58:03 +1200

    npc

--- scripts/levels/npc.gd
+++ scripts/levels/npc.gd
@@ -2,18 +2,24 @@ extends CharacterBody2D
 
 @onready var nav_agent = $NavigationAgent2D
 const SPEED = 300
+#
+#var min = -1280
+#var corner_one = Vector2i(-1280, 960)
+#var corner_two = Vector2i(2400, -2720)
+#var width = abs(corner_one.x - corner_two.x)
+#var height = abs(corner_one.y - corner_two.y)
 
+var bottom_left = Vector2i(-1280, 960) #whoops these shouldnt be vectors
+var bottom_right = Vector2i(4480, 960)
+var top_left = Vector2i(-1280, -2720)
+var top_right = Vector2i(4480, -2720)
 
-var corner_one = Vector2i(1050, -1750)
-var corner_two = Vector2i(2390, -170)
-var width = abs(corner_one.x - corner_two.x)
-var height = abs(corner_one.y - corner_two.y)
 
 func _ready() -> void:
 	nav_agent.navigation_finished.connect(on_nav_finished)
 	nav_agent.velocity_computed.connect(_on_navigation_agent_2d_velocity_computed)
-	make_path(Vector2(randf_range(1050,width), randf_range(2390,height)))
-
+	make_path(Vector2(randi_range(bottom_left,bottom_right), randi_range(top_left,top_right))) #-1280, 960 # 4480, -2720
+	
 func _physics_process(delta: float) -> void:
 	var next_path_pos = nav_agent.get_next_path_position()
 	var direction = global_position.direction_to(next_path_pos)
@@ -22,7 +28,7 @@ func _physics_process(delta: float) -> void:
 	nav_agent.velocity = new_velocity
 
 func on_nav_finished():
-	make_path(Vector2(randf_range(1050,width), randf_range(2390,height)))
+	make_path(Vector2(randi_range(bottom_left,bottom_right), randi_range(top_left,top_right)))
 	
 func _on_navigation_agent_2d_velocity_computed(safe_velocity):
 	velocity = velocity.move_toward(safe_velocity, 100)
--- scripts/levels/outside.gd
+++ scripts/levels/outside.gd
@@ -8,68 +8,67 @@ func _ready() -> void:
 	elif globals.player_loading_point == "store":
 		player.global_position = Vector2(2198.0, 500)
 		
-	$Tilemaps/Collisions.hide()
+	$Tilemaps/Tilemaps/Collisions.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	#print(player.global_position.y)
-	print(get_global_mouse_position())
 	# Make the bottom layer fade out as you move upwards
 	if player.global_position.y < -700:
-		if $"Tilemaps/Bottom Layer/Buildings".modulate.a > 0.2:
-			$"Tilemaps/Bottom Layer/Buildings".modulate.a -= 0.03
+		if $"Tilemaps/Tilemaps/Bottom Layer/Buildings".modulate.a > 0.2:
+			$"Tilemaps/Tilemaps/Bottom Layer/Buildings".modulate.a -= 0.03
 			
-		if $"Tilemaps/Bottom Layer/Buildings Bottom".modulate.a > 0.2:
-			$"Tilemaps/Bottom Layer/Buildings Bottom".modulate.a -= 0.03
+		if $"Tilemaps/Tilemaps/Bottom Layer/Buildings Bottom".modulate.a > 0.2:
+			$"Tilemaps/Tilemaps/Bottom Layer/Buildings Bottom".modulate.a -= 0.03
 			
 	else:
-		if $"Tilemaps/Bottom Layer/Buildings".modulate.a < 1:
-			$"Tilemaps/Bottom Layer/Buildings".modulate.a += 0.03
+		if $"Tilemaps/Tilemaps/Bottom Layer/Buildings".modulate.a < 1:
+			$"Tilemaps/Tilemaps/Bottom Layer/Buildings".modulate.a += 0.03
 			
-		if $"Tilemaps/Bottom Layer/Buildings Bottom".modulate.a < 1:
-			$"Tilemaps/Bottom Layer/Buildings Bottom".modulate.a += 0.03
+		if $"Tilemaps/Tilemaps/Bottom Layer/Buildings Bottom".modulate.a < 1:
+			$"Tilemaps/Tilemaps/Bottom Layer/Buildings Bottom".modulate.a += 0.03
 	
 	# Make the second layer fade out as you move upwards
 	if player.global_position.y < -1024:
-		if $"Tilemaps/Second Layer/Buildings2".modulate.a > 0.2:
-			$"Tilemaps/Second Layer/Buildings2".modulate.a -= 0.03
+		if $"Tilemaps/Tilemaps/Second Layer/Buildings2".modulate.a > 0.2:
+			$"Tilemaps/Tilemaps/Second Layer/Buildings2".modulate.a -= 0.03
 			
-		if $"Tilemaps/Second Layer/Buildings2 Bottom".modulate.a > 0.2:
-			$"Tilemaps/Second Layer/Buildings2 Bottom".modulate.a -= 0.03
+		if $"Tilemaps/Tilemaps/Second Layer/Buildings2 Bottom".modulate.a > 0.2:
+			$"Tilemaps/Tilemaps/Second Layer/Buildings2 Bottom".modulate.a -= 0.03
 			
 	else:
-		if $"Tilemaps/Second Layer/Buildings2".modulate.a < 1:
-			$"Tilemaps/Second Layer/Buildings2".modulate.a += 0.03
+		if $"Tilemaps/Tilemaps/Second Layer/Buildings2".modulate.a < 1:
+			$"Tilemaps/Tilemaps/Second Layer/Buildings2".modulate.a += 0.03
 			
-		if $"Tilemaps/Second Layer/Buildings2 Bottom".modulate.a < 1:
-			$"Tilemaps/Second Layer/Buildings2 Bottom".modulate.a += 0.03
+		if $"Tilemaps/Tilemaps/Second Layer/Buildings2 Bottom".modulate.a < 1:
+			$"Tilemaps/Tilemaps/Second Layer/Buildings2 Bottom".modulate.a += 0.03
 	
 	# Make the third layer fade out as you move upwards
 	if player.global_position.y < -1584:
-		if $"Tilemaps/Third Layer/Buildings3".modulate.a > 0.2:
-			$"Tilemaps/Third Layer/Buildings3".modulate.a -= 0.03
+		if $"Tilemaps/Tilemaps/Third Layer/Buildings3".modulate.a > 0.2:
+			$"Tilemaps/Tilemaps/Third Layer/Buildings3".modulate.a -= 0.03
 			
-		if $"Tilemaps/Third Layer/Buildings3 Bottom".modulate.a > 0.2:
-			$"Tilemaps/Third Layer/Buildings3 Bottom".modulate.a -= 0.03
+		if $"Tilemaps/Tilemaps/Third Layer/Buildings3 Bottom".modulate.a > 0.2:
+			$"Tilemaps/Tilemaps/Third Layer/Buildings3 Bottom".modulate.a -= 0.03
 			
 	else:
-		if $"Tilemaps/Third Layer/Buildings3".modulate.a < 1:
-			$"Tilemaps/Third Layer/Buildings3".modulate.a += 0.03
+		if $"Tilemaps/Tilemaps/Third Layer/Buildings3".modulate.a < 1:
+			$"Tilemaps/Tilemaps/Third Layer/Buildings3".modulate.a += 0.03
 			
-		if $"Tilemaps/Third Layer/Buildings3 Bottom".modulate.a < 1:
-			$"Tilemaps/Third Layer/Buildings3 Bottom".modulate.a += 0.03
+		if $"Tilemaps/Tilemaps/Third Layer/Buildings3 Bottom".modulate.a < 1:
+			$"Tilemaps/Tilemaps/Third Layer/Buildings3 Bottom".modulate.a += 0.03
 			
 	# Make the fourth layer fade out as you move upwards
 	if player.global_position.y < -2464:
-		if $"Tilemaps/Fourth Layer/Buildings4".modulate.a > 0.2:
-			$"Tilemaps/Fourth Layer/Buildings4".modulate.a -= 0.03
+		if $"Tilemaps/Tilemaps/Fourth Layer/Buildings4".modulate.a > 0.2:
+			$"Tilemaps/Tilemaps/Fourth Layer/Buildings4".modulate.a -= 0.03
 			
-		if $"Tilemaps/Fourth Layer/Buildings4 Bottom".modulate.a > 0.2:
-			$"Tilemaps/Fourth Layer/Buildings4 Bottom".modulate.a -= 0.03
+		if $"Tilemaps/Tilemaps/Fourth Layer/Buildings4 Bottom".modulate.a > 0.2:
+			$"Tilemaps/Tilemaps/Fourth Layer/Buildings4 Bottom".modulate.a -= 0.03
 			
 	else:
-		if $"Tilemaps/Fourth Layer/Buildings4".modulate.a < 1:
-			$"Tilemaps/Fourth Layer/Buildings4".modulate.a += 0.03
+		if $"Tilemaps/Tilemaps/Fourth Layer/Buildings4".modulate.a < 1:
+			$"Tilemaps/Tilemaps/Fourth Layer/Buildings4".modulate.a += 0.03
 			
-		if $"Tilemaps/Fourth Layer/Buildings4 Bottom".modulate.a < 1:
-			$"Tilemaps/Fourth Layer/Buildings4 Bottom".modulate.a += 0.03
+		if $"Tilemaps/Tilemaps/Fourth Layer/Buildings4 Bottom".modulate.a < 1:
+			$"Tilemaps/Tilemaps/Fourth Layer/Buildings4 Bottom".modulate.a += 0.03
