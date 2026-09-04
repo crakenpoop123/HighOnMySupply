d275afd087bf724b54856738530edaf5a76697b1
crakenpoop123 <gerracl@whs.school.nz>
2026-09-04 12:53:43 +1200

    Insufficient Stock Errors + inventory integration
     - A red error shows on screen for 2 seconds when you do not have the stock  to interact with the crusher or shredder minigames
     - The shredder minigame now takes sugar_cane from your inventory and gives you shredded_cane

--- scripts/buildables/crusher.gd
+++ scripts/buildables/crusher.gd
@@ -11,5 +11,10 @@ func _process(delta: float) -> void:
 	pass
 
 func player_interact():
+	# If the player has at least one shredded cane
 	if globals.inventory_ingredients["shredded_cane"]["stock"] > 0:
+		# Change the scene to the crushing minigame
 		globals.change_scene(true, "res://scenes/minigames/crush_minigame.tscn")
+	else:
+		# Return the name of shredded cane
+		return globals.inventory_ingredients["shredded_cane"]["name"]
