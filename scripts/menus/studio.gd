458a42755a408cbacafc0b859ee5e823421e7c52
krannnnn <kierancollins@outlook.co.nz>
2026-09-04 14:10:13 +1200

    moved around files

--- scripts/menus/studio.gd
+++ scripts/menus/studio.gd
@@ -1,4 +1,4 @@
 extends CanvasLayer
 
 func _on_timer_timeout() -> void: # CHANGE SCENE
-	get_tree().change_scene_to_file("res://scenes/menus/main_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/gui/menus/main_menu.tscn")
