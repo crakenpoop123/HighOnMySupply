8cc46e33fc4810c10e97f16f3a6cff470eb3146a
crakenpoop123 <gerracl@whs.school.nz>
2026-09-04 15:17:49 +1200

    boil minigame stuffffffffffff

--- scripts/buildables/boiler.gd
+++ scripts/buildables/boiler.gd
@@ -6,7 +6,7 @@ func player_interact():
 	# If the player has at least one cane juice
 	if globals.inventory_ingredients["cane_juice"]["stock"] > 0:
 		# Change the scene to the boiling minigame
-		globals.change_scene(true, "res://scenes/minigames/boil_minigame.tscn") # TODO: Create this minigame
+		globals.change_scene(true, "res://scenes/minigames/boil_minigame.tscn")
 	else:
 		# Return the name of cane juice
 		return globals.inventory_ingredients["cane_juice"]["name"]
---
+++ scripts/minigames/boil_minigame.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+var boiling = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Boiler/LiquidInBoiler.visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if boiling:
+		# Move the liquid into the boiler
+		$Boiler/LiquidInBoiler.visible = true
+		$Ingredients/CaneJuice.visible = false
+		
+		# Play the boiler animation
+		$Boiler/BoilerSprite.play()
+	else:
+		# Stop the boiler animation
+		$Boiler/BoilerSprite.stop()
+		# Set the animation to the first frame
+		$Boiler/BoilerSprite.frame = 0
+
+
+func _on_boiler_area_area_entered(area: Area2D) -> void:
+	if area.name == "CaneJuice":
+		globals.can_drag = false
+		boiling = true
