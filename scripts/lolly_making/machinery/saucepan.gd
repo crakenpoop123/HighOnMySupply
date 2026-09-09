ed5eef8a58f551838aeae95e26c903a92c73418c
Clark Gerrard <71289244+crakenpoop123@users.noreply.github.com>
2026-09-10 09:31:22 +1200

    Shrunk saucepan hitbox
     - Did this for the minigame version
     - This forces the ingredients to be closer to the center of the saucepan

--- scripts/lolly_making/machinery/saucepan.gd
+++ scripts/lolly_making/machinery/saucepan.gd
@@ -1,23 +1,22 @@
 extends StaticBody2D
 
+# Changed this from a global var to a local one
 var saucepan_on_table = true
-
-func _process(_delta: float) -> void:
-	pass
-	#if globals.saucepan_on_table == true: # CHANGE LOCATION DEPENDING ON STATE
-		#$".".position = Vector2(-650, -43)
-	#elif globals.saucepan_on_table == false:
-		#$".".position = Vector2(-752, -28)
 
 
 func player_interact():
 	if saucepan_on_table == true: # CHANGE SCENE DEPENDING ON STATE
 		if (globals.inventory_ingredients["sugar"]["stock"] > 0 # Check you have enough ingredients
 		 and globals.inventory_ingredients["gelatin"]["stock"] > 0):
+			# Decrease items from inventory
 			globals.inventory_ingredients["sugar"]["stock"] -= 1
 			globals.inventory_ingredients["gelatin"]["stock"] -= 1
+			
+			# Change the scene
 			globals.change_scene(true, "res://scenes/minigames/gummy_worm_minigame.tscn")
+		
 		else: # Return the necessary ingredients, so that the user knows what they need to get
 			return globals.inventory_ingredients["sugar"]["name"] + " or " + globals.inventory_ingredients["gelatin"]["name"]
-	elif saucepan_on_table == false:
+	
+	else: # saucepan_on_table == false
 		globals.change_scene(true, "res://scenes/minigames/gummy_cooking_minigame.tscn")
