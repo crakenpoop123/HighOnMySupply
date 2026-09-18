9e3c4b69cf2e60c9d8f6c7c9a8fcf81203957a0a
hydeza <hydeza@whs.school.nz>
2026-09-18 15:00:16 +1200

    good

--- scripts/lolly_making/machinery/saucepan.gd
+++ scripts/lolly_making/machinery/saucepan.gd
@@ -1,12 +1,11 @@
 extends StaticBody2D
 
 # Changed this from a global var to a local one
-var saucepan_on_table = true
 var readable_name = "Saucepan"
 
 
 func player_interact():
-	if saucepan_on_table == true: # CHANGE SCENE DEPENDING ON STATE
+	if globals.saucepan_on_table == true: # CHANGE SCENE DEPENDING ON STATE
 		if (globals.inventory_ingredients["sugar"]["stock"] > 0 # Check you have enough ingredients
 		 and globals.inventory_ingredients["gelatin"]["stock"] > 0):
 			# Decrease items from inventory
@@ -19,5 +18,5 @@ func player_interact():
 		else: # Return the necessary ingredients, so that the user knows what they need to get
 			return globals.inventory_ingredients["sugar"]["name"] + " or " + globals.inventory_ingredients["gelatin"]["name"]
 	
-	else: # saucepan_on_table == false
+	else:
 		globals.change_scene(true, "res://scenes/minigames/gummy_cooking_minigame.tscn")
