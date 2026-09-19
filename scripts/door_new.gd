4d6509520542e111230dde72fd3008db4131e953
crakenpoop123 <clarkgerrard2@gmail.com>
2026-09-19 17:31:51 +1200

    tryna fix outside.tscn

--- scripts/door_new.gd
+++ scripts/door_new.gd
@@ -4,5 +4,5 @@ func _ready() -> void:
 	pass
 	
 func player_interact():
-	globals.change_scene(true, $"..".scene_to_load)
 	globals.player_loading_point = get_parent().scene_from
+	globals.change_scene(true, $"..".scene_to_load)
