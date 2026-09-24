895d350df19500a5708e5b21b359cc5de7247f16
krannnnn <kierancollins@outlook.co.nz>
2026-09-25 10:37:12 +1200

    fixes

--- scripts/menus/rainbow.gd
+++ scripts/menus/rainbow.gd
@@ -1,4 +1,8 @@
 extends Control
 
+func _ready() -> void:
+	$Rainbow.visible = false
+
 func play_animation():
+	$Rainbow.visible = true
 	$RainbowAnimation.play("RainbowSpin") # Plays the rainbow spinning animation when its called from elsewhere
