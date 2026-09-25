b7ab1b123ca10d93ac63ee57e659178c3f86cbab
krannnnn <kierancollins@outlook.co.nz>
2026-09-25 13:07:59 +1200

    tutorial

--- scripts/menus/tutorial.gd
+++ scripts/menus/tutorial.gd
@@ -11,7 +11,7 @@ Once placed you can press E to interact
 You can also interact with npcs with E
 ",
 
- "Tutorial - Sugar
+"Tutorial - Sugar
 Go to the grass
 Place a farm plot and water it to speed it up
 Cut it down
@@ -23,7 +23,7 @@ Bring the wet sugar to the dryer
 Congrats, you have sugar!
 ",
 
- "Tutorial - Gummy worms
+"Tutorial - Gummy worms
 Once you have made sugar go to the store and buy some gelatin
 The store is to the left of the basement
 Put the sugar, gelatin, and water into a saucepan
@@ -32,24 +32,30 @@ In the next minigame take it off of the stove and pour it into the mold
 Congrats, you have gummy worms!
 ",
 
-"Tutorial - should probably do one for selling them too maybe"
+"Tutorial - Selling gummy worms
+Once you have gummy worms you need to sell them
+You can do that by walking up to any npc and interacting with them
+Once you've interacted with them you'll have an option to sell them some gummy worms
+The price changes every now and then so make sure to buy low and sell high!
+"
 ]
 
 func _process(delta: float) -> void:
-	$RichTextLabel.text = text_array[menu]
-	if menu != 3:
-		$Button2.visible = true
-	else:
-		$Button2.visible = false
-	if menu != 0:
-		$Button3.visible = true
-	else:
-		$Button3.visible = false
-func _on_button_button_up() -> void:
+	$RichTextLabel.text = text_array[menu] # set the text to the right page
+	if menu != 3: # If you are on a page you can go forwards on
+		$NextButton.visible = true # Show next button
+	else: # If you are on a page you can't go forwards on
+		$NextButton.visible = false # Hide next button
+	if menu != 0: # If you are on a page you can go back on
+		$BackButton.visible = true # Show back button
+	else: # If you aren't on a page you can go back on
+		$BackButton.visible = false #Hide back button
+		
+func _on_button_button_up() -> void: # Destroy self when closed
 	self.queue_free()
 
-func _on_button_2_button_up() -> void:
+func _on_button_2_button_up() -> void: #Increase menu by one
 	menu += 1
 
-func _on_button_3_button_up() -> void:
+func _on_button_3_button_up() -> void: # Decrease menu by one
 	menu -= 1
